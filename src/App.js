8f591b605653a5ef96e44ee6277865351138edce
Tero The Cat <furryalt582@gmail.com>
2024-06-02 16:28:30 -0600

    flex layout with sample text

--- src/App.js
+++ src/App.js
@@ -1,14 +1,70 @@
-function Title() {
+import cat1 from './images/cat285.jpg'
+import './App.css';
+
+function App(){
   return(
-    <div className="TitleBar">
-      <h1>Home</h1>
-      <h1>About</h1>
+    <div className="App">
+      <Title />
+      <Header />
+      <div class="row">
+        <div className="column middle">
+          <Articles />
+        </div>
+        <div className="column side">
+          <Trend />
+        </div>
+      </div>
+    </div>
+  );  
+  
+}
+
+function Title(){
+  return (
+    <div className="App-title">
+      <h1>WetFeed</h1>
+    </div>
+  );
+}
+
+function Trend(){
+  return(
+    <div className="Trends">
+      <h2>Trending Now</h2>
+      <article>
+      <img src={cat1} alt="cat" className="Trend"/>
+      <span> Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur cupidatat.</span>
+      </article>
+    </div>
+  );
+}
+
+function Articles(){
+  return(
+  <table>
+    <tr>
+      <th><img src={cat1} alt="cat" className="Article" /></th>
+      <th>Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur cupidatat.</th>
+    </tr>
+  </table>  
+
+  );
+}
+
+function Header() {
+  return(
+    <div className="topnav">
+      
+        <a href="" target="_blank">Home</a>
+        <a href="" target="_blank">Quizzes</a>
+        <a href="" target="_blank">Shopping</a>
+        <a href="" target="_blank">Articles</a>
     </div>
   );
   
 }
 export default function Main(){
   return(
-    <Title />
+    <App />
   );
 }
